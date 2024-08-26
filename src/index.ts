c0b7a3bd31140d8208a465aade630f9e74323858
coderxuz <coderxuz2009@gmail.com>
2024-08-26 15:34:25 +0300

    app added

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,3 @@
 import './index.scss'
-const h1 = document.querySelector('h1')!
-const click = document.querySelector('.click')! as HTMLDivElement
-console.log(click);
+import axios from 'axios'
 
-function local():number{
-    const getting = localStorage.getItem('count')
-    if(getting){
-        const parsed:number = JSON.parse(getting)
-        if(parsed && typeof parsed === 'number'){
-            return parsed
-        }
-    }
-    return 0
-}
-click.addEventListener('click', (e)=>{
-    e.preventDefault()
-    let count:number = local()
-    h1.innerHTML = count.toString()
-    count++
-    localStorage.setItem('count', JSON.stringify(count))
-})
-window.addEventListener('DOMContentLoaded', ()=>{
-    let count:number = local()
-    h1.innerHTML = JSON.stringify(count)
-})
